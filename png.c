2b0fecef323c372be4ac1d5f95b0098aa6e951f6
deadman96385 <seanhoyt963@gmail.com>
2015-01-01 04:07:49 -0500

    Kill more spam
    Change-Id: I4736b976ecc137f395a015b84dfc66c8f1324b9f
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- png.c
+++ png.c
@@ -2276,9 +2276,8 @@ png_compare_ICC_profile_with_sRGB(png_const_structrp png_ptr,
                       * problems if they are used, therefore attempt to
                       * discourage their use, skip the 'have_md5' warning below,
                       * which is made irrelevant by this error.
-                      */
                      png_chunk_report(png_ptr, "known incorrect sRGB profile",
-                        PNG_CHUNK_ERROR);
+                        PNG_CHUNK_ERROR); */
                   }
 
                   /* Warn that this being done; this isn't even an error since
